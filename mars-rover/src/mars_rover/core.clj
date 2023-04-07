a4c9b2703f1ad36674a3da9db73eb12615d9bb15
shahn95 <shahn95@gmail.com>
2023-04-07 13:13:37 +0530

    Use nested transforms map with symbols where possible + Modify core functions to work with this new structure
    Using one nested transforms map with nested left, right and move keys.
    Using symbols as values instead of strings to remove symbol conversion
    at a later stage where possible.
    Modified core functions to work with nested maps and symbols directly.

--- mars-rover/src/mars_rover/core.clj
+++ mars-rover/src/mars_rover/core.clj
@@ -4,6 +4,8 @@
   (:require [clojure.string :as string])
   (:gen-class))
 
+(declare rotate-left, rotate-right, move, transforms, fn-map)
+
 ;; todo: input validation, check invalid moves
 
 (defn user-input
@@ -21,44 +23,41 @@
              :direction rover-direction}
      :cmd-sequence cmd-sequence}))
 
-(def transform-left {"N" "W"
-                     "E" "N"
-                     "S" "E"
-                     "W" "S"})
-
-(def transform-right {"N" "E"
-                      "E" "S"
-                      "S" "W"
-                      "W" "N"})
-
-(def transform-move {"N" "y inc"
-                     "E" "x inc"
-                     "S" "y dec"
-                     "W" "x dec"})
-
-(def fn-map {"L" "rotate-left"
-             "R" "rotate-right"
-             "M" "move"})
-
 (defn rotate-left
   [info-map]
-  (assoc-in info-map [:rover :direction] (get transform-left (get-in info-map [:rover :direction]))))
+  (assoc-in info-map [:rover :direction] (get-in transforms [:left (get-in info-map [:rover :direction])])))
 
 (defn rotate-right
   [info-map]
-  (assoc-in info-map [:rover :direction] (get transform-right (get-in info-map [:rover :direction]))))
+  (assoc-in info-map [:rover :direction] (get-in transforms [:right (get-in info-map [:rover :direction])])))
 
 (defn move
   [info-map]
-  (let [transformation (get transform-move (get-in info-map [:rover :direction]))
-        [key op] (string/split transformation #" ")]
-    (assoc-in info-map [:rover (keyword key)] ((resolve (symbol op)) (get-in info-map [:rover (keyword key)])))))
+  (let [[key op] (get-in transforms [:move (get-in info-map [:rover :direction])])] 
+    (assoc-in info-map [:rover key] ((resolve op) (get-in info-map [:rover key])))))
 
 (defn process-cmds
   [info-map]
-  (reduce (fn [rover-map rover-fn] ((resolve (symbol (get fn-map rover-fn))) rover-map))
+  (reduce (fn [new-info-map cmd] ((get fn-map cmd) new-info-map))
           info-map
           (map str (get info-map :cmd-sequence))))
+
+(def transforms {:left {"N" "W"
+                        "E" "N"
+                        "S" "E"
+                        "W" "S"}
+                 :right {"N" "E"
+                         "E" "S"
+                         "S" "W"
+                         "W" "N"}
+                 :move {"N" '(:y inc)
+                        "E" '(:x inc)
+                        "S" '(:y dec)
+                        "W" '(:x dec)}})
+
+(def fn-map {"L" mars-rover.core/rotate-left
+             "R" mars-rover.core/rotate-right
+             "M" mars-rover.core/move})
 
 (defn -main
   [& args]
@@ -69,6 +68,6 @@
 
 ;; (def info-map {:plateau-size {:x 10 :y 10}
 ;;                :rover {:x 0 :y 0 :direction "N"}
-;;                :cmd-sequence "LLRRMM"})
+;;                :cmd-sequence "LLRMMLLMMLMM"})
 ;; info-map
 ;; (process-cmds info-map)
