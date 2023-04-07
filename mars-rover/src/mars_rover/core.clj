a165db12b79e32f1b50ab1d06daa5172e3c6ccdc
shahn95 <shahn95@gmail.com>
2023-04-07 16:30:19 +0530

    Break down user input function into smaller functions
    Created smaller functions to read and process each line of user input.
    Will add error checking to these individual functions.

--- mars-rover/src/mars_rover/core.clj
+++ mars-rover/src/mars_rover/core.clj
@@ -1,47 +1,68 @@
 ;; Mars Rover Problem
 
+;; todo: input validation, check invalid moves
+
 (ns mars-rover.core
   (:require [clojure.string :as string])
   (:gen-class))
 
-(declare rotate-left, rotate-right, move, transforms, fn-map)
+(declare get-initial-position, get-cmd-sequence, transforms, fn-map)
 
-;; todo: input validation, check invalid moves
-
-(defn user-input
-  "Waits for 3 lines of input from user"
+(defn get-input
   []
-  (let [plateau-size (string/trim (read-line))
-        [plateau-size-x plateau-size-y] (map #(Integer/parseInt %) (string/split plateau-size #" "))
-        rover-position (string/trim (read-line))
-        [rover-x rover-y rover-direction] (string/split rover-position #" ")
-        cmd-sequence (string/trim (read-line))]
-    {:plateau-size {:x plateau-size-x
-                    :y plateau-size-y}
-     :rover {:x (Integer/parseInt rover-x)
-             :y (Integer/parseInt rover-y)
-             :direction rover-direction}
-     :cmd-sequence cmd-sequence}))
+  (let [input (string/trim (read-line))]
+    (string/upper-case input)))
+
+(defn get-plateau-size
+  "Read one line of input stream to get upper right coordinates from user"
+  []
+  (let [xy-string (get-input)
+        [x y] (map #(Integer/parseInt %) (string/split xy-string #" "))]
+    (get-initial-position {:plateau-size {:x x
+                                          :y y}})))
+
+(defn get-initial-position
+  "Read one line of input stream to get initial rover position"
+  [info-map]
+  (let [intial-position (get-input)
+        [x y d] (string/split intial-position #" ")]
+    (get-cmd-sequence (assoc-in info-map [:rover] {:x (Integer/parseInt x)
+                                                   :y (Integer/parseInt y)
+                                                   :direction d}))))
+
+(defn get-cmd-sequence
+  "Read one line of input stream to get sequence of commands for rover movement"
+  [info-map]
+  (let [cmd-sequence (get-input)]
+    (assoc-in info-map [:cmd-sequence] cmd-sequence)))
 
 (defn rotate-left
+  "Rotate rover counter-clockwise 90 degrees"
   [info-map]
   (assoc-in info-map [:rover :direction] (get-in transforms [:left (get-in info-map [:rover :direction])])))
 
 (defn rotate-right
+  "Rotate rover clockwise 90 degrees"
   [info-map]
   (assoc-in info-map [:rover :direction] (get-in transforms [:right (get-in info-map [:rover :direction])])))
 
 (defn move
+  "Move rover forward one unit"
   [info-map]
-  (let [[key op] (get-in transforms [:move (get-in info-map [:rover :direction])])] 
+  (let [[key op] (get-in transforms [:move (get-in info-map [:rover :direction])])]
     (assoc-in info-map [:rover key] ((resolve op) (get-in info-map [:rover key])))))
 
+
 (defn process-cmds
+  "Reduces over a list of functions and builds up the rover's final position"
+  ;; Uses fn-map to get the core functions corresponding to L, R and M commands
   [info-map]
   (reduce (fn [new-info-map cmd] ((get fn-map cmd) new-info-map))
           info-map
           (map str (get info-map :cmd-sequence))))
 
+;; Map defining left, right and move transforms
+;; key -> current location; value -> location after applying transform
 (def transforms {:left {"N" "W"
                         "E" "N"
                         "S" "E"
@@ -55,6 +76,7 @@
                         "S" '(:y dec)
                         "W" '(:x dec)}})
 
+;; Mapping of L, R and M to their respective core functions
 (def fn-map {"L" mars-rover.core/rotate-left
              "R" mars-rover.core/rotate-right
              "M" mars-rover.core/move})
@@ -62,7 +84,7 @@
 (defn -main
   [& args]
   (println "Please enter input:")
-  (println (process-cmds (user-input))))
+  (println (process-cmds (get-plateau-size))))
 
 ;; Testing
 
