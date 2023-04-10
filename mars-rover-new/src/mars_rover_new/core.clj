c72e3801bb6c65a5b6118bd1e49c5705cf578474
shahn95 <shahn95@gmail.com>
2023-04-10 15:41:51 +0530

    Get input from user and store in info-map
    Added functionality to accept 3 lines of input data.
    Data is then decomposed and stored in a map: info-map.

--- mars-rover-new/src/mars_rover_new/core.clj
+++ mars-rover-new/src/mars_rover_new/core.clj
@@ -1,7 +1,64 @@
+;; Mars Rover Exercise
+
 (ns mars-rover-new.core
+  (:require [clojure.string :as string])
   (:gen-class))
 
+;; todo: input validation, check invalid moves
+
+(declare add-plateau-size, add-rover-position, add-cmd-sequence)
+
+(def info-map {:plateau-size {:x nil
+                              :y nil}
+               :rover-position {:x nil
+                                :y nil
+                                :direction nil}
+               :cmd-sequence nil})
+
+(defn get-input
+  "Read one line of input stream. Trim and convert to upper-case."
+  []
+  (let [input (string/trim (read-line))]
+    (string/upper-case input)))
+
+(defn user-input
+  "Reads 3 lines of input from user and calls 3 functions which add this data to info-map"
+  []
+  (println "Please enter plateau-size, initial rover position and command sequence on 3 separate lines:")
+  (let [plateau-size (get-input)
+        rover-position (get-input)
+        cmd-sequence (get-input)]
+    (add-cmd-sequence cmd-sequence (add-rover-position rover-position (add-plateau-size plateau-size info-map)))))
+
+(defn add-plateau-size
+  "Add plateau-size data to info-map"
+  [plateau-size, info-map]
+  (let [[x y] (map #(Integer/parseInt %) (string/split plateau-size #" "))]
+    (assoc-in info-map [:plateau-size] {:x x
+                                        :y y})))
+
+(defn add-rover-position
+  "Add rover-position data to info-map"
+  [rover-position, info-map]
+  (let [[x y d] (string/split rover-position #" ")]
+    (assoc-in info-map [:rover-position] {:x (Integer/parseInt x)
+                                 :y (Integer/parseInt y)
+                                 :direction d})))
+
+(defn add-cmd-sequence
+  "Add cmd-sequence data to info-map"
+  [cmd-sequence, info-map]
+  (assoc-in info-map [:cmd-sequence] cmd-sequence))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Program entry point"
   [& args]
-  (println "Hello, World!"))
+  (println "Welcome to Mars!")
+  (prn (user-input)))
+
+;; repl testing
+;; (add-plateau-size "10 100" info-map)
+;; (add-initial-position "0 0 N" info-map)
+;; (add-cmd-sequence "LLRRMM" info-map)
+
+;; (add-cmd-sequence "LLRRMM" (add-initial-position "0 0 N" (add-plateau-size "10 10" info-map)))
