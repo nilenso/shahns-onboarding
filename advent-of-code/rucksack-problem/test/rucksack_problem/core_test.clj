e889c47bab5db110742669a28eeed970939cfc51
shahn95 <shahn@nilenso.com>
2023-05-23 22:12:29 +0530

    Refactor compartmentalize and sum-rucksack-priorities tests

--- advent-of-code/rucksack-problem/test/rucksack_problem/core_test.clj
+++ advent-of-code/rucksack-problem/test/rucksack_problem/core_test.clj
@@ -1,19 +1,17 @@
 (ns rucksack-problem.core-test
   (:require [clojure.test :refer :all]
-            [rucksack-problem.core :refer :all]))
-
-;;      First version: Process a single rucksack
-;;   1. Divide rucksack string into 2 halves
-;;   2. Check for the common item (only one exists) in both halves
-;;   3. Find the priority of the common item using the below relationships:
-;;      Priority of a - z: 1 - 26
-;;      Priority of A - Z: 27 - 52)
-;;
-;;      And then expand to multiple rucksacks
+            [rucksack-problem.core :refer :all])
+  (:gen-class))
 
 (deftest sum-rucksack-priorities-test
   (testing "Return the sum of common-item-priority for each rucksack in the supplied input list"
-    (is (= 157 (sum-rucksack-priorities input-list)))))
+    (let [test-input '("vJrwpWtwJgWrhcsFMMfFFhFp"
+                       "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+                       "PmmdzqPrVvPwwTWBwg"
+                       "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+                       "ttgJtRGJQctTZtZT"
+                       "CrZsJsPPZsGzwwsLwLmpwMDw")]
+      (is (= 157 (sum-rucksack-priorities test-input))))))
 
 (deftest common-item-priority-test
   (testing "Return the priority of the common item between the 2 compartments of a rucksack"
@@ -22,8 +20,11 @@
 
 (deftest compartmentalize-test
   (testing "Divide rucksack contents into 2 halves"
-    (is (= '("vJrwpWtwJgWr" "hcsFMMfFFhFp")
-           (compartmentalize "vJrwpWtwJgWrhcsFMMfFFhFp")))))
+    (let [rucksack "vJrwpWtwJgWrhcsFMMfFFhFp"
+          first-half '(\v \J \r \w \p \W \t \w \J \g \W \r)
+          second-half '(\h \c \s \F \M \M \f \F \F \h \F \p)]
+      (is (= (list first-half second-half)
+             (compartmentalize rucksack))))))
 
 (deftest common-item-test
   (testing "Return the common element between the 2 compartments"
@@ -38,6 +39,8 @@
       1 (calculate-priority \a)
       26 (calculate-priority \z)
       27 (calculate-priority \A)
+      52 (calculate-priority \Z))))
+
 (deftest find-badge-test
   (testing "Return the common item (badge) between a group of 3 rucksacks"
     (let [rucksack-1 "vJrwpWtwJgWrhcsFMMfFFhFp"
